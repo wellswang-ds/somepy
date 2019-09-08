e2ad8d136a713e617dcabc0d6a0b887fd3a40d7a
wellsytheman <wee3wells@gmail.com>
2019-09-07 20:44:01 -0700

    test2

--- fromjj/challenge_project.py
+++ fromjj/challenge_project.py
@@ -3,21 +3,7 @@ import webbrowser
 import time
 import numpy
 
-def draw_square(some_turtle):
-    for i in range (1,5):
-        some_turtle.forward(100)
-        some_turtle.right(90)
 
-def draw_circle(some_turtle):
-    some_turtle.circle(100)
-
-def draw_circle2(some_turtle):
-    some_turtle.circle(125)
-
-def draw_triangle(some_turtle):
-    for i in range(1,4):
-        some_turtle.forward(250)
-        some_turtle.right(120)
 
 def draw_art():
 
