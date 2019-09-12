7891aaa09b873e9436a9c6970441220048a11ac4
wellsytheman <wee3wells@gmail.com>
2019-09-11 22:53:38 -0700

    update list-comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,6 +2,11 @@
 #'[[1, 2, 3], [4, 5, 6], [7, 8, 9]]'
 
 a = [1,2,3]
-b = [[i+3*j for i in a] for j in range(3)]
+b = [[i+3*(j-1) for i in a] for j in a]
 
 print(b)
+
+
+# a = [0,1,2]
+# b = [[i+3*j+1 for i in a] for j in a]
+# print(b)
