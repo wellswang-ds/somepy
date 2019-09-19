98f9330f3e34ec3546701e583262d05dab2e1d5a
wellsytheman <wee3wells@gmail.com>
2019-09-19 16:58:37 -0700

    add multiprocessing

--- wellsytheman/py_test/group_by.py
+++ wellsytheman/py_test/group_by.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-path = '/Users/wells_wang/PycharmProjects/Python/wellsytheman/py_test/data'
+path = '/Users/wells_wang/PycharmProjects/Python/wellsytheman/py_test/data/nba.csv'
 files = pd.read_csv(path,header=0)
 print(files.head())
 
-g = files.groupby('Team')
-BC = g.get_group('Boston Celtics').Name
+g = files.groupby('Team').Name
+BC = g.get_group('Boston Celtics')
+gg = list(g.groups.keys())
+del(files)
 print(BC)
+# CC = []
+# for i in gg[3:]:
+#     a = list(g.get_group(i))
+#     CC.append(a)
+# print(CC)
