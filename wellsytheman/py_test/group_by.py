3659cc00be3b2b257aa9025af4e4f37de18f7f95
wellsytheman <wee3wells@gmail.com>
2019-09-20 16:18:16 -0700

    update multiprocessing.py

--- wellsytheman/py_test/group_by.py
+++ wellsytheman/py_test/group_by.py
@@ -1,14 +1,26 @@
 import pandas as pd
-
+import numpy as np
 path = '/Users/wells_wang/PycharmProjects/Python/wellsytheman/py_test/data/nba.csv'
 files = pd.read_csv(path,header=0)
-print(files.head())
-
+# print(files.head())
+files = files.sort_values('Salary')
+# print(files)
 g = files.groupby('Team').Name
-BC = g.get_group('Boston Celtics')
+# for name, group in g:
+#     print(name)
+# BC = g.get_group()
 gg = list(g.groups.keys())
+
+df = pd.DataFrame(g.agg(lambda x: ','.join(x)))
+df.reset_index(level=0, inplace=True)
+aa = df.Name.iloc[4]
+a = list(aa.split(","))
+print(aa)
+print(a)
+# print(df)
+# print(gg[7])
 del(files)
-print(BC)
+# print(BC)
 # CC = []
 # for i in gg[3:]:
 #     a = list(g.get_group(i))
