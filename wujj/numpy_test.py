494ffbc11bc37fa2176da54c4d4fe1ef6f8c631b
wellsytheman <wee3wells@gmail.com>
2019-09-24 15:09:26 -0700

    g8y

--- wujj/numpy_test.py
+++ wujj/numpy_test.py
@@ -4,18 +4,32 @@
 import csv
 import numpy as np
 
-csvfile=open('/Users/wells_wang/Desktop/mpg.csv') # open the file into a variable
+csvfile= open('/Users/wells_wang/Desktop/mpg.csv') # open the file into a variable
 mpg = list(csv.reader(csvfile)) # read the file as csv and then transfer it to a list
 mpg[:3] # check out the first three items in our list.
 
 # 1. We do not need the column headings as the first item. Lets move the headings to another variable.
 
-headings = mpg[0]
-mpg = mpg[1:]
-print(headings)
-print(mpg)
+# headings = mpg[0]
+# mpg = mpg[1:]
+# print(headings)
+# print(mpg)
 
 # 2. Try transforming the mpg dataset to NumPy array. What happens?
-# Ans: we
-# npa = np.asarray(mpg)
-# print(npa)
+        #Integer becomes string
+    # Fix the issue by moving the name column to another list.
+    # You may do this any way you can and print out both the names and the mpg datasets.
+    # Can you do it with list comprehension?
+    #  [x for b in a for x in b]
+# mpg3 = [None for i in mpg for j in i if '?' in j]
+
+print(mpg3)
+npa = np.array(mpg3)
+print(mpg)
+print(npa)
+
+# with open('/Users/wells_wang/Desktop/mpg.csv') as csvfile:
+#     file = csv.reader(csvfile)
+#     for row in file:
+#         print(row)
+# print(csv.reader(csvfile))
